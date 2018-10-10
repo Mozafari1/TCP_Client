bbf4cd6f4a3d71b325c386d9827a53c5f81b1738
Rahmat Mozafari <38719925+Mozafari1@users.noreply.github.com>
2018-10-10 13:22:34 +0200

    Update client.c

--- TCP_Client/client.c
+++ TCP_Client/client.c
@@ -3,7 +3,7 @@
 #include<io.h>
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
-#pragma warning(disable : 4996)
+#pragma warning(disable : 4996) // Deaktiverer link feilen
 
 int main(int argc, char *argv[])
 {
@@ -25,9 +25,9 @@ int main(int argc, char *argv[])
 		printf("Kunne ikke lage socket: %d", WSAGetLastError());
 	}
 	printf("Socket var laget\n");	
-	client.sin_addr.s_addr = inet_addr("158.36.70.56");
+	client.sin_addr.s_addr = inet_addr("ENTER YOUR IP");
 	client.sin_family = AF_INET;
-	client.sin_port = htons(80);
+	client.sin_port = htons(ENTER YOUR PORT);
 	//Connect to remote server
 	if (connect(s, (struct sockaddr *)&client, sizeof(client)) < 0)
 	{
@@ -35,17 +35,17 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	puts("Koblet");
-	// arrayet jeg sender skal se slik ut [ meldings ID, data type av short, medlingings størrelse, student nr]
+	// arrayet jeg sender skal se slik ut [ meldings ID, data type av short, medlingings stÃ¸rrelse, student nr]
 	// setter bufferet til null
 
 	memset(server_reply, 512,0);
-	// meldingens id og den ligger på første plassen
-	server_reply[0] = 0x01;// arryet ser slik ut [0x01]
-	// lager en meldings størrelse i short med 7 plasser
-	unsigned M_lengde = htons((short)0x07);
+	// meldingens id og den ligger pÃ¥ fÃ¸rste plassen
+	server_reply[0] = 0x1;// arryet ser slik ut [0x01]
+	// lager en meldings stÃ¸rrelse i short med 7 plasser
+	unsigned M_lengde = htons((short)7);
 	// kopierer en block med data fra en kilde address til en plassering address.
-	memcpy(server_reply+1, &M_lengde, 0x02);// arryet ser slik ut [0x01,0x00, 0x07]
-	memcpy(server_reply+4, message, 0x06); // arryet ser slik ut [0x01,0x00,0x07,0x06,0,1,2,4,5,6]
+	memcpy(server_reply+1, &M_lengde,2);// arryet ser slik ut [0x01,0x00, 0x07]
+	memcpy(server_reply+4, message,6); // arryet ser slik ut [0x01,0x00,0x07,0x06,0,1,2,4,5,6]
 
 	recv_size = send(s, server_reply,10, 0);// bruker 10 fordi lengde til bufferet 10 
 	if (recv_size<0) {
